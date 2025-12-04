347aff07a05472352a66f331b22fd1cbf40b116e
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-12-03 23:22:07 -0500

    updates for core file sync

---
+++ files-to-sync/services/ui-auth/handlers/createUsers.js
@@ -0,0 +1,47 @@
+// This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
+import * as cognitolib from "../libs/cognito-lib.js";
+import users from "../libs/users.json" assert { type: "json" };
+const userPoolId = process.env.userPoolId;
+
+export const handler = async (_event, _context, _callback) => {
+  for (var i = 0; i < users.length; i++) {
+    var poolData = {
+      UserPoolId: userPoolId,
+      Username: users[i].username,
+      MessageAction: "SUPPRESS",
+      UserAttributes: users[i].attributes,
+    };
+    var passwordData = {
+      Password: process.env.bootstrapUsersPassword,
+      UserPoolId: userPoolId,
+      Username: users[i].username,
+      Permanent: true,
+    };
+    var attributeData = {
+      Username: users[i].username,
+      UserPoolId: userPoolId,
+      UserAttributes: users[i].attributes,
+    };
+
+    try {
+      // This may error if the user already exists
+      await cognitolib.createUser(poolData);
+    } catch {
+      /* swallow this exception and continue */
+    }
+
+    try {
+      //userCreate must set a temp password first, calling setPassword to set the password for consistent dev login
+      await cognitolib.setPassword(passwordData);
+    } catch {
+      /* swallow this exception and continue */
+    }
+
+    try {
+      //if user exists and attributes are updated in this file updateUserAttributes is needed to update the attributes
+      await cognitolib.updateUserAttributes(attributeData);
+    } catch {
+      /* swallow this exception and continue */
+    }
+  }
+};
