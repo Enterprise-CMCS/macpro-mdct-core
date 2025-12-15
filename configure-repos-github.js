7611f59edddb3ab32fd5bd2a077ae3109c041d87
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-12-15 13:33:08 -0500

    remove filter, running configure-repos on all apps

--- configure-repos-github.js
+++ configure-repos-github.js
@@ -15,7 +15,6 @@ const CONFIG_PATH = path.join(ROOT_DIR, "repo-settings.json");
 const SEPARATOR_WIDTH = 80;
 
 const REPO_CONFIG = (await loadReposFromConfig(true))
-  .filter((r) => r.includes("carts") || r.includes("mfp") || r.includes("seds"))
   .reduce((acc, repo) => {
     acc[repo] = { dryRun: false };
     return acc;
