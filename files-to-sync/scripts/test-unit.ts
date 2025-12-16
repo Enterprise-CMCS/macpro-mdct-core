485d0dcc4dea6293fd9fcb19b72d61ebde027e74
Pete Dunlap <pete.dunlap@stratiform.digital>
2025-12-16 15:41:30 -0500

    CMDCT-5445 converting shell to ts

---
+++ files-to-sync/scripts/test-unit.ts
@@ -0,0 +1,18 @@
+// This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
+// node scripts/test-unit.ts
+import { execSync } from "node:child_process";
+import { chdir, cwd } from "node:process";
+
+let yarnCommand = "yarn install";
+if (process.env.CI === "true") {
+  yarnCommand = "yarn install --frozen-lockfile";
+}
+
+execSync(yarnCommand, { stdio: "inherit" });
+for (const service of ["ui-src", "app-api"]) {
+  const originalDir = cwd();
+  chdir(`services/${service}`);
+  execSync(yarnCommand, { stdio: "inherit" });
+  execSync("yarn run coverage", { stdio: "inherit" });
+  chdir(originalDir);
+}
