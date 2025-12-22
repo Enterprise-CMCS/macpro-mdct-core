d33c2a052fea54c45048aeddbfdba49b7aa217ff
Pete Dunlap <pete.dunlap@stratiform.digital>
2025-12-22 15:29:21 -0500

    CMDCT-5465 syncs writing ui env file (#158)

---
+++ files-to-sync/cli/lib/write-ui-env-file.ts
@@ -0,0 +1,26 @@
+// This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
+import path, { dirname } from "node:path";
+import { promises as fs } from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const configFilePath = path.resolve(
+  path.join(__dirname, "../../services/ui-src/public/env-config.js")
+);
+
+export const writeLocalUiEnvFile = async (
+  envVariables: Record<string, string>
+) => {
+  await fs.rm(configFilePath, { force: true });
+
+  const envConfigContent = [
+    "window._env_ = {",
+    ...Object.entries(envVariables).map(
+      ([key, value]) => `  ${key}: "${value}",`
+    ),
+    "};",
+  ].join("\n");
+
+  await fs.writeFile(configFilePath, envConfigContent);
+};
