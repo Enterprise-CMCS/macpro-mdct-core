5000e9a51251e068fd5aa4da91f94189a34fc6fb
Pete Dunlap <pete.dunlap@stratiform.digital>
2026-01-06 10:04:55 -0500

    CMDCT-5467 mostly use yarn vs alternatives (#162)

--- types/existing-modules.d.ts
+++ types/existing-modules.d.ts
@@ -3,6 +3,6 @@
  * By declaring them here we can get typescript to stop throwing `TS2307: Cannot find module`
  * errors, in a spot where we know that's the case and is our intention.
  */
-declare module "*/deployment-config.js";
-declare module "*/parent.js";
+declare module "*/deployment-config.ts";
+declare module "*/parent.ts";
 declare module "*/utils.ts";
